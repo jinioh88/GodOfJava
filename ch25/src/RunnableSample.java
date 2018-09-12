9efb27d7ad8dc1aacd8ff4bff0ac449dc0bba4be
오세진 <jinioh88@gmail.com>
2018-09-12 14:13:29 +0900

    쓰레드

---
+++ ch25/src/RunnableSample.java
@@ -0,0 +1,5 @@
+public class RunnableSample implements Runnable {
+    public void run() {
+        System.out.println("THis is Runnable's run() method.");
+    }
+}
---
+++ ch25/src/ThreadSample.java
@@ -0,0 +1,5 @@
+public class ThreadSample extends Thread {
+    public void run() {
+        System.out.println("This is ThreadSample's run method.");
+    }
+}
