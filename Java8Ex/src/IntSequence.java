0fc3661c3afee9970498a22f0aa5629d8625dc8f
오세진 <jinioh88@gmail.com>
2018-10-03 13:51:03 +0900

    람다

--- Java8Ex/src/IntSequence.java
+++ Java8Ex/src/IntSequence.java
@@ -2,4 +2,6 @@ public interface IntSequence {
     public static IntSequence digitsOf(int n) {
         return new DigitSequence(n);
     }
+
+    void intInit(int n);
 }
--- Java8Ex/src/IntegerTest.java
+++ Java8Ex/src/IntegerTest.java
@@ -1,4 +1,14 @@
+import java.util.Random;
+
 public class IntegerTest {
+    private static Random generator = new Random();
+
+    public static IntSequence randomInts(int low, int hight) {
+        return (n) -> {
+            System.out.println(n);
+        };
+    }
+
     public static void main(String[] args) {
         EnumSingleton singleton = EnumSingleton.INSTANCE;
         System.out.println(singleton.hashCode());
